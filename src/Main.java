c65f6e5031218fdf1655941c67d65bd5d7a854a1
MESKI HAMZA <88968389+HAMZA7onx@users.noreply.github.com>
2024-09-19 23:09:24 +0200

    TCNSTRCTN-20 manage project

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello friend");
+        // test comment
 
-        for(byte i = 0; i < 2;i ++) {
-
-        }
     }
  }
